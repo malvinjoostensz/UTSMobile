d806655bb1e5109f83eadd2b132d6c3aad804a67
malvinjoostensz <joostenszmalvin@gmail.com>
2020-10-17 11:38:58 +0700

    admins.page.html

---
+++ src/app/components/modalcard/modalcard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalcard',
+  templateUrl: './modalcard.component.html',
+  styleUrls: ['./modalcard.component.scss'],
+})
+export class ModalcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/modaledit/modaledit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modaledit',
+  templateUrl: './modaledit.component.html',
+  styleUrls: ['./modaledit.component.scss'],
+})
+export class ModaleditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
